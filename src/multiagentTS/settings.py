8347c2cbe0a7fee26563e9fd778c9e0f8c8ba507
nicopernas <nicopernas@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-11-15 18:27:23 +0000

    Primera versión del proyecto

--- src/multiagentTS/settings.py
+++ src/multiagentTS/settings.py
@@ -93,7 +93,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    '$USER_ROOT/templates',
+    #'$USER_ROOT/templates',
 )
 
 INSTALLED_APPS = (
