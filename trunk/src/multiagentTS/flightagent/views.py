26a48a294942c9e45e64b87c32467d73ea2a2658
nicopernas <nicopernas@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-11-24 19:31:51 +0000

    Agrego el fichero sql

--- trunk/src/multiagentTS/flightagent/views.py
+++ trunk/src/multiagentTS/flightagent/views.py
@@ -5,10 +5,13 @@ Created on 15/11/2010
 '''
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from multiagentTS.flightagent.models import FlightModel
+from multiagentTS.flightagent.models import *
 from multiagentTS.kayak import start_flight_search
 
 def index(request):
+	countrymodels = CountryModels.objects.all()
+	
+	'''
 	flightsearch = start_flight_search('n', 'MVD', 'TCI', '12/04/2010', '12/07/2010', 'a', 'a', '1', 'e', '10')
 	if flightsearch is None:
 		print "Mandar el error al template..."
@@ -16,6 +19,6 @@ def index(request):
 		flights = []
 		for f in flightsearch:
 			flights.append(FlightModel(f))
-			
-		return render_to_response('flightagent_show_flights.html', {'flights': flights },
+	'''	
+	return render_to_response('flightagent_show_flights.html', {'country': countrymodels },
 								context_instance = RequestContext(request))
