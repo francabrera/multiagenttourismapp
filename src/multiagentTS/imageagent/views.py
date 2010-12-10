8c4def23b0ce4d1f5e41c5464d0740a46e5d6f38
romenrg@gmail.com <romenrg@gmail.com@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-10 16:28:46 +0000

    Image Agent funcionando. Recibe 4 parámetros pero sólo utiliza el último, que es la ciudad destino

--- src/multiagentTS/imageagent/views.py
+++ src/multiagentTS/imageagent/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.template import RequestContext
 
-def showImages (request):
-    return render_to_response('image.html')
+def showImages (request, pais_origen, ciudad_origen, pais_destino, ciudad_destino):
+    
+    return render_to_response('image.html', {'destino': ciudad_destino})
