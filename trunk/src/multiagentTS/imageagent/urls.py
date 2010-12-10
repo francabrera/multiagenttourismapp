479d545f39970a1e430e72579901d7bd8edc438e
romenrg@gmail.com <romenrg@gmail.com@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-10 16:28:46 +0000

    Image Agent funcionando. Recibe 4 parámetros pero sólo utiliza el último, que es la ciudad destino

--- trunk/src/multiagentTS/imageagent/urls.py
+++ trunk/src/multiagentTS/imageagent/urls.py
@@ -5,6 +5,5 @@ from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 
 urlpatterns = patterns('imageagent.views',
-
-    (r'^$', 'showImages'),
+    (r'^(?P<pais_origen>[ \w]+)/(?P<ciudad_origen>[ \w]+)/to/(?P<pais_destino>[ \w]+)/(?P<ciudad_destino>[ \w]+)$', 'showImages'),
 )
