860b4ec941ac0bd415d0be683b4871c3b0aa264b
nicopernas <nicopernas@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-13 22:38:02 +0000

    Casi terminado lo de los hoteles y vuelos

--- src/multiagentTS/flightagent/views.py
+++ src/multiagentTS/flightagent/views.py
@@ -19,4 +19,20 @@ def index(request, airportOrigen, fechaSalida, airportDestino, fechaRegreso):
 		for f in flightsearch:
 			flights.append(FlightModel(f))
 	return render_to_response('flightagent_show_flights.html', {'flights': flights },
-							context_instance = RequestContext(request))
+					context_instance = RequestContext(request))
+
+
+def empty_url(request):
+	
+	html = ''' 
+		<h3>Para realizar una b&uacute;squeda sobre un vuelo debe especificar:</h3>
+		<ul>
+			<li>C&oacute;digo IATA del aeropuerto de salida.</li>
+			<li>Fecha de salida con el formato MM/DD/AAAA.</li>
+			<li>C&oacute;digo IATA del aeropuerto de llegada.</li>
+			<li>Fecha de llegada con el formato MM/DD/AAAA.</li>				
+		</ul>
+		<span>Por ejemplo: http://servidor/flight/MAD/12-20-2010/to/JFK/12-24-2010</span>
+	'''
+	return render_to_response('show_error_empty_url.html', {'message': html },
+						context_instance = RequestContext(request))
--- src/multiagentTS/hotelagent/views.py
+++ src/multiagentTS/hotelagent/views.py
@@ -8,14 +8,10 @@ from django.template import RequestContext
 from multiagentTS.hotelagent.models import HotelModel
 from multiagentTS.kayak import start_hotel_search
 
-def index(request):
-    othercity = 'Montevideo,UY'
-    checkin_date = '01/15/2011'
-    checkout_date = '01/25/2019'
-    guests1 = '1'
-    rooms = '1'
-
-    hotelsearch = start_hotel_search(othercity, checkin_date, checkout_date, guests1, rooms, '3', 'normal', 'down', 'price')
+def index(request, othercity, checkin_date, checkout_date):
+    checkin_date = checkin_date.replace('-','/')
+    checkout_date = checkout_date.replace('-','/')
+    hotelsearch = start_hotel_search(othercity, checkin_date, checkout_date, '1', '1', '3', 'normal', 'down', 'price')
 
     hotels = []
     if hotelsearch is not None:
@@ -23,3 +19,17 @@ def index(request):
             hotels.append(HotelModel(h))
     return render_to_response('hotelagent_show_hotels.html', {'hotels': hotels },
                             context_instance = RequestContext(request))
+
+def empty_url(request):
+    
+    html = ''' 
+        <h3>Para realizar una b&uacute;squeda sobre un hotel, debe especificar:</h3>
+        <ul>
+            <li>Nombre de la ciudad y c&oacute;digo ISO del pa&iacute;s donde se va a hospedar, separados por una coma.</li>
+            <li>Fecha de inicio de la estad&iacute;a con el formato MM/DD/AAAA.</li>
+            <li>Fecha de finalizaci&oacute;n de la estad&iacute;a con el formato MM/DD/AAAA.</li>
+        </ul>
+        <span>Por ejemplo: http://servidor/hotel/Madrid,ES/12-20-2010/to/12-24-2010<span>
+    '''
+    return render_to_response('show_error_empty_url.html', {'message': html },
+                        context_instance = RequestContext(request))
