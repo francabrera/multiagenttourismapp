23bc380dd260dab2ab10509517feb07d377ee731
nicopernas <nicopernas@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-13 22:38:02 +0000

    Casi terminado lo de los hoteles y vuelos

--- trunk/src/multiagentTS/flightagent/urls.py
+++ trunk/src/multiagentTS/flightagent/urls.py
@@ -11,5 +11,7 @@ from django.conf import settings
 urlpatterns = patterns('flightagent.views',
 
 #	('n', airportOrigen, airportDestino, fechaSalida, fechaRegreso, 'a', 'a', '1', 'e', '5')
+	(r'^$', 'empty_url'),	
 	(r'^(?P<airportOrigen>\w{3})/(?P<fechaSalida>\d{2}-\d{2}-\d{4})/to/(?P<airportDestino>\w{3})/(?P<fechaRegreso>\d{2}-\d{2}-\d{4})$', 'index'),
+
 )
--- trunk/src/multiagentTS/hotelagent/urls.py
+++ trunk/src/multiagentTS/hotelagent/urls.py
@@ -11,5 +11,7 @@ from django.conf import settings
 urlpatterns = patterns('hotelagent.views',
 
 #	('n', airportOrigen, airportDestino, fechaSalida, fechaRegreso, 'a', 'a', '1', 'e', '5')
-	(r'^$', 'index'),
+	(r'^$', 'empty_url'),
+	(r'^(?P<othercity>\w*,\w*)/(?P<checkin_date>\d{2}-\d{2}-\d{4})/to/(?P<checkout_date>\d{2}-\d{2}-\d{4})$', 'index'),
+
 )
