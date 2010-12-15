67e7fbd0ec4094e6929d133b02492e19254a01de
franxu <franxu@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-15 20:08:24 +0000

    Agregando parámetros obtenidos del formulario y creando urls

--- src/multiagentTS/locationagent/views.py
+++ src/multiagentTS/locationagent/views.py
@@ -3,8 +3,13 @@ from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect, HttpResponse, HttpRequest
 from django.template import RequestContext
 
-def showMap (request, pais_origen, ciudad_origen, pais_destino, ciudad_destino):    
-    flighturl = 'http://' + request.get_host() + '/flight/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
+def showMap (request, pais_origen, ciudad_origen, pais_destino, ciudad_destino): 
+    fechaSalida = request.session['fecha_salida']
+    sfechaSalida = fechaSalida.strftime('%m-%d-%Y')
+    fechaLlegada = request.session['fecha_llegada']
+    sfechaLlegada= fechaLlegada.strftime('%m-%d-%Y')
+    flighturl = 'http://' + request.get_host() + '/flight/' + ciudad_origen + '/' + \
+                sfechaSalida + '/to/' + ciudad_destino + '/' + sfechaLlegada
     imageurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
     hotelurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
     guideurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
