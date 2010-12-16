af08ddea4d822706186e7365992f12aef88f46e7
romenrg@gmail.com <romenrg@gmail.com@0918fab6-b9f6-8048-c67b-47b22cc8b660>
2010-12-16 16:13:41 +0000

    Final!

--- trunk/src/multiagentTS/locationagent/views.py
+++ trunk/src/multiagentTS/locationagent/views.py
@@ -8,14 +8,21 @@ def showMap (request, pais_origen, ciudad_origen, pais_destino, ciudad_destino):
     sfechaSalida = fechaSalida.strftime('%m-%d-%Y')
     fechaLlegada = request.session['fecha_llegada']
     sfechaLlegada= fechaLlegada.strftime('%m-%d-%Y')
+    nombrePaisDestino = request.session['pais_destino']
+    nombrePaisOrigen = request.session['pais_origen']
+    nombreCiudadDestino = request.session['ciudad_destino']
+    nombreCiudadOrigen = request.session['ciudad_origen']
+    
     flighturl = 'http://' + request.get_host() + '/flight/' + ciudad_origen + '/' + \
                 sfechaSalida + '/to/' + ciudad_destino + '/' + sfechaLlegada
-    imageurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
-    hotelurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
-    guideurl = 'http://' + request.get_host() + '/image/' + pais_origen + '/' + ciudad_origen + '/to/' + pais_destino + '/' + ciudad_destino
+    imageurl = 'http://' + request.get_host() + '/image/' + nombrePaisOrigen + '/' + nombreCiudadOrigen + '/to/' + nombrePaisDestino + '/' + nombreCiudadDestino
+    hotelurl = 'http://' + request.get_host() + '/hotel/' + nombreCiudadDestino + ',' + pais_destino + '/' + \
+                sfechaSalida + '/to/' + sfechaLlegada
+    guideurl = 'http://' + request.get_host() + '/guide/' + nombrePaisOrigen + '/' + nombreCiudadOrigen + '/to/' + nombrePaisDestino + '/' + nombreCiudadDestino
+    
     context = {'flight_url': flighturl, 'image_url': imageurl,
-               'hotel_url': hotelurl, 'guide_url': guideurl}
-    return render_to_response('location.html', {'pais_origen': pais_origen,
-                                                'pais_destino': pais_destino,
-                                                'ciudad_origen': ciudad_origen,
-                                                'ciudad_destino': ciudad_destino})
+               'hotel_url': hotelurl, 'guide_url': guideurl,
+               'pais_origen': pais_origen, 'pais_destino': pais_destino,
+               'ciudad_origen': ciudad_origen, 'ciudad_destino': ciudad_destino}
+    
+    return render_to_response('location.html', context)
